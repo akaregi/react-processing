9b77ab9bb8f2d26113fb50e5a6f1fbdcff9ea86c
akaregi <verdigris@azure.icu>
2022-07-17 04:20:20 +0900

    add: description intensifies
    Signed-off-by: akaregi <verdigris@azure.icu>

--- src/components/app.tsx
+++ src/components/app.tsx
@@ -22,15 +22,20 @@ header, footer {
   margin: 3rem 0;
 }
 
-hr {
-  margin: 3rem 0;
-  border-width: 0;
-  border-top: 1px dotted #30b8cd;
+h2 {
+  margin-top: 2rem;
+  padding-top: 0.5rem;
+  border-top: 1px solid #ccc;
 }
 
 section {
   display: flex;
   justify-content: center;
+}
+
+p {
+  margin: 0.5rem;
+  text-indent: 1rem;
 }
 
 canvas.p5Canvas {
@@ -42,30 +47,71 @@ const App = () => {
   return (
     <>
       <GlobalStyle />
-
       <header>
         <h1>Processing(p5.js) with React</h1>
       </header>
-
       <p>Hyperthink intensifies</p>
-
+      <p>
+        <strong>¡¡¡あんた超注意!!!</strong>&nbsp;
+        このサイトはスマホで遊ぶことをウンボのウの字ほどにも考えておらず、スマホで遊ぼうとするとあんたは
+        &nbsp;<i>INHALE INTENSIFIES</i>{" "}
+        効果を付与されてしまいます。パソコンないしパソコン相当の何某で遊んでみてください。
+      </p>
+      <p>
+        <strong>
+          レンダリング面積が大きい作品は折りたたまれているので、適量展開してください。
+        </strong>
+      </p>
       <h2>Trochoid</h2>
-      <Canvas sketch={trochoid} />
-
+      <p>
+        p5.jsの入門にとトロコイドとかいう普通に数学的に難しい内容を扱ったので出来がかなり悪い
+      </p>
+      <details>
+        <summary>出来が悪いので格納</summary>
+        <Canvas sketch={trochoid} />
+      </details>
       <h2>Mouse circle</h2>
+      <p>
+        ドラッグし続けるといい感じのほわほわが出てきて消えちゃいます。ドラッグする意味ある？っていう哲学的問いをあんたに押し付けるものです。
+      </p>
       <Canvas sketch={mouseCircle} />
-
       <h2>Noise Graph</h2>
+      <p>
+        パーリンノイズに沿ってグラフ状に乱数を生成し続けるやつです。パーリンノイズは連続してるふうに見える乱数を生成してくれるので、グラフも一様乱数だと滅茶苦茶なのがこのグラフは連続してるように見えるはずです。
+      </p>
+      <p>
+        工夫すればろローソク足チャートめいたの作れそうだよね。近く作ります。ところで何故蝋燭（ろうそく）ではなくローソクなのか？これも調べる必要がありますがここでは扱いたくない。
+      </p>
       <Canvas sketch={noiseGraph} />
-
       <h2>Line Circle</h2>
-      <p>Enter to redraw</p>
-      <Canvas sketch={lineCircle} />
-
+      <p>
+        なんというか見ての通りのアレとしか言いようがありません。円形に生成したパーリンノイズのそれと言ったら通じるかな？通じたらとても嬉しい。
+      </p>
+      <p>
+        Enterで無限に再生成できます。しかし覚えておかなければなりません。一度生成して保存しなかった画像はF5なりEnterを押すと消えてしまい二度と蘇ることがありません。本当に押しますか？それを？
+      </p>
+      <details>
+        <summary>大きいので格納</summary>
+        <Canvas sketch={lineCircle} />
+      </details>
       <h2>Circle Cycle</h2>
       <p>
-        注意:
+        周期性のあるパーリンノイズというのを扱いたくて設計したものです。まず、普通のパーリンノイズは当然ながら特定地点で同じ数列が繰り返されるということはないんですよ。何が問題かって、
+        <i>Line Circle</i>&nbsp;
+        みたいに円形に組む時、支点と終点が連続しないので乱数生成によっては断崖絶壁が生じて見栄えが良くない。
+      </p>
+      <p>
+        で、ref
+        に示している通りパーリンノイズの乱数源に円を用いると周期性のあるノイズを作れるっていう話があった。円は一周回れば元の位置に戻るので当然周期性がある。これを上手いこと組み合わせれば周期性のあるパーリンノイズが実現できるっていう話らしいんですが。私はアヒルなんでよくわかりませんでした。
+      </p>
+      <p>
+        <strong>注意:</strong>&nbsp;
         Enterで画像を無限に吸えるものの、乱数生成が悪いのかF5しないと似たような画像が生成されます。趣向を変えたい場合F5してください。変えたくない場合はF5を押さないでください。
+      </p>
+      <p>
+        マジでなんかこう、一通り乱数生成を中心に調べてみたんだけど、
+        <code>Math.random</code>
+        の値によって外向き内向きに棒が伸びるとかそういった法則は見いだせず、そもそもコードの数式全体を雰囲気で処理しているので何処が悪いのかいまいち判然としない。GitHub（フッターにリンクがある）からヘルプを私に投げる権利が貴方に留保されています。
       </p>
       <p>
         ref:&nbsp;
@@ -73,8 +119,10 @@ const App = () => {
           周期的パーリンノイズ
         </a>
       </p>
-      <Canvas sketch={circleCycle} />
-
+      <details>
+        <summary>大きいので格納</summary>
+        <Canvas sketch={circleCycle} />
+      </details>
       <footer>
         <p>
           (C) 2022 nanigashi. Apache-2.0 and CC BY-SA 4.0.{" "}
